6f435f0b65e5a347d4bb1536794ec2306b677317
Sergio (Admethius) <sergio.cp@hotmail.es>
2022-08-24 17:41:31 -0400

    nuevos objetos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PaisModule } from './c03_pais/pais/pais.module';
 import { AppRoutingPaisModule } from './pais.routing';
+import { BancoTablaComponent } from './listador/componentes/banco-tabla/banco-tabla.component';
+import { PokemonTablaComponent } from './listador/componentes/pokemon-tabla/pokemon-tabla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BancoTablaComponent,
+    PokemonTablaComponent
   ],
   imports: [
     BrowserModule,
--- src/app/c03_pais/pais/page-pais/page-pais.component.ts
+++ src/app/c03_pais/pais/page-pais/page-pais.component.ts
@@ -1,5 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-pais',
